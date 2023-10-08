3e131b4aeba0a23963aeecf9b36450675d46b142
Omorisiagbon Abraham <abrahamosazee3@gmail.com>
2023-10-08 22:05:56 +0100

    fix file structure

--- frontend/vite-number-conversion/src/Routes.tsx
+++ frontend/vite-number-conversion/src/Routes.tsx
@@ -7,7 +7,7 @@ import {
 
 import Home from "./pages/Home/Home";
 import LandingPage from "./pages/Home/LandingPage/LandingPage";
-import NoMatch from "./pages/Home/NoMatch/NoMatch";
+import NoMatch from "./pages/NoMatch/NoMatch";
 
 function Router() {
   const router = createBrowserRouter(
--- frontend/vite-number-conversion/src/pages/NoMatch/NoMatch.tsx
+++ frontend/vite-number-conversion/src/pages/NoMatch/NoMatch.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../../assets/404_page.png";
+import Logo from "../../assets/404_page.png";
 import "./NoMatch.css";
 import { useNavigate } from "react-router-dom";
 
